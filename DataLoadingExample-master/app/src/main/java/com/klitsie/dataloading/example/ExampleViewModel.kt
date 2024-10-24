99ad340a72beb64ca9270e10d82ed036401df567
Tam Pham <tam.pham@limeade.com>
2024-10-24 09:35:42 +0700

    Add DataLoadingExample prject

---
+++ DataLoadingExample-master/app/src/main/java/com/klitsie/dataloading/example/ExampleViewModel.kt
@@ -0,0 +1,58 @@
+package com.klitsie.dataloading.example
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.klitsie.dataloading.RefreshTrigger
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+@Immutable
+sealed interface ExampleEvent {
+
+	data object ShowRefreshFailure : ExampleEvent
+
+}
+
+@Stable
+class ExampleViewModel(
+	exampleDataLoader: ExampleDataLoader = DefaultExampleDataLoader(),
+	private val exampleDataMapper: ExampleDataMapper = ExampleDataMapper,
+	private val refreshTrigger: RefreshTrigger = RefreshTrigger(),
+) : ViewModel() {
+
+	private val _event = MutableStateFlow<ExampleEvent?>(null)
+	val event = _event.asStateFlow()
+
+	private val data = exampleDataLoader.loadAndObserveData(
+		coroutineScope = viewModelScope,
+		refreshTrigger = refreshTrigger,
+		onRefreshFailure = { throwable ->
+			println(throwable)
+			_event.update { ExampleEvent.ShowRefreshFailure }
+		},
+	)
+
+	val screenState = data.map { exampleDataMapper.map(it) }.stateIn(
+		scope = viewModelScope,
+		started = SharingStarted.WhileSubscribed(),
+		initialValue = exampleDataMapper.map(data.value),
+	)
+
+	fun refresh() {
+		viewModelScope.launch {
+			refreshTrigger.refresh()
+		}
+	}
+
+	fun consumeEvent() {
+		_event.update { null }
+	}
+
+}
